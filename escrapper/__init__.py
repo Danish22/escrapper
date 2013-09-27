712308fddf336e5b86c01d0efd10bebf24471b4f
Espartaco Palma <esparta@gmail.com>
2013-09-27 18:41:17 -0500

    Bump to v.0.1.0

--- escrapper/__init__.py
+++ escrapper/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __all__ = ['WebSVN','InvalidWebSVN',]
 __author__ = "Espartaco Palma"
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 from .websvn import WebSVN
 from .exceptions import *
